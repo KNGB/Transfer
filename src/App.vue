36a1338ccc28d771db19900aff47f6fcdcfb765f
kangb <kangbing1997@163.com>
2020-12-15 16:42:22 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <mode></mode>
+    <inputEnter></inputEnter>
+  </div>
+</template>
+
+<script>
+
+import mode from './mode'
+import inputEnter from './inputEnter'
+export default {
+  name: 'App',
+  components: {
+   mode,
+   inputEnter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/inputEnter.vue
@@ -0,0 +1,65 @@
+<template>
+
+<div>
+    <el-input v-on:keyup.enter.native="keydown($event)" class="123"> </el-input>
+    <el-input v-on:keyup.enter.native="keydown($event)" class="123"> </el-input>
+    <el-input v-on:keyup.enter.native="keydown($event)" class="123"> </el-input>
+    <el-input v-on:keyup.enter.native="keydown($event)" class="123"> </el-input>
+<input type="text" class="123" v-on:keyup.enter="keydown($event)" ><br>
+<input type="text" class="123" v-on:keyup.enter="keydown"><br>
+<input type="text" class="123" ><br>
+<input type="text" class="123" ><br>
+<input type="text" class="123" ><br>
+<input type="text" class="123" ><br>
+<input type="text" class="123" ><br>
+</div>
+
+</template>
+
+<script>
+export default {
+    name: "inputEnter",
+    components: {},
+    // extends: null,
+    // minxins: [],
+    props: [],
+    data() {
+        return {
+
+        };
+    },
+    computed: {},
+    watch: {},
+    beforeCreate() {},
+    created() {},
+    beforeMount() {},
+    mounted() {},
+    beforeUpdate() {},
+    updated() {},
+    beforeDestroy() {},
+    destroyed() {},
+    methods: {
+        keydown(e){
+            console.log(e)
+            
+           let nodes =  document.getElementsByClassName('123');
+           for (let index = 0; index < nodes.length; index++) {
+               const element = nodes[index].children[0];
+               console.log(element)
+               if(element===e.target){
+                   console.log(index)
+                   if(index!==nodes.length-1){
+                       nodes[index+1].children[0].focus();
+                   }
+               }
+           }
+           
+           
+        }
+    }
+};
+</script>
+
+<style lang="less" scoped>
+
+</style>
